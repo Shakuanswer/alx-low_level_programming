b9905ea67d3e1e2001fe374e46ecf6b502e40959
Shaku_Karabo <shakuanswer@gmail.com>
2023-02-16 15:41:31 -0800

    sizw

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,17 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - A program that prints the size of various computer types
+ * Return 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	float f;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a);
+printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b);
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c);
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(d);
+return (0);
+}
