add09aaabc397d2c68873a6b6bae132dde78c77f
Shaku_Karabo <shakuanswer@gmail.com>
2023-02-16 15:06:27 -0800

    H print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - A program that prints lines using printf function
+ * Return: 0 (correct)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
