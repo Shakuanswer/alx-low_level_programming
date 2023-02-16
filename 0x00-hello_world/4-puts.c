ad61d995a346549f6eaeb60a94044430c7a054c7
Shaku_Karabo <shakuanswer@gmail.com>
2023-02-16 14:56:26 -0800

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - A program that prints lines using puts function
+ * Return: 0 (correct)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
