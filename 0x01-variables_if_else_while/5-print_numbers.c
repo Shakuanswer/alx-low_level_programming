0846c20d64185ca3f90611f2a473509589572004
Shaku_Karabo <shakuanswer@gmail.com>
2023-02-17 14:57:02 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints all strings from 0 to 0
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
